488d2a2e19bedf859bc03108ef72dbefaa46e79f
Rawnak Tasnim Ruku <tasnim_ruku1152@uits.edu.bd>
2026-01-12 22:31:19 +0600

    Add files via upload

---
+++ Emergency Services BD/ofrs/index.php
@@ -0,0 +1,45 @@
+<?php include_once('includes/config.php');?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Emergency Services BD</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Responsive navbar-->
+<?php include_once('includes/header.php') ?>
+        <!-- Page Content-->
+        <div class="container px-4 px-lg-5">
+            <!-- Heading Row-->
+            <div class="row gx-4 gx-lg-5 align-items-center my-5">
+                <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="assets/11623.jpg" alt="..." /></div>
+                <div class="col-lg-5">
+                    <h1 class="font-weight-light">Emergency Services BD</h1>
+                    <p><b>Emergency Service BD</b> is an online platform that provides quick access to all essential emergency services across Bangladesh. It connects users with ambulance services, fire departments, police assistance, medical emergencies, and disaster response teams. The platform ensures fast and efficient coordination through real-time tracking, instant alerts, 
+                        and a user-friendly interface, making emergency assistance more accessible and reliable.</p>
+                    <a class="btn btn-primary" href="reporting.php">Emergency Reporting</a>
+                </div>
+            </div>
+            <!-- Call to Action-->
+            <div class="card text-white bg-secondary my-5 py-4 text-center">
+                <div class="card-body"><p class="text-white m-0"> Your one-stop platform for all emergency services in Bangladesh. Access fire safety, medical ambulance, and police assistance anytime. Stay informed, stay safe! 🚑🔥👮‍♂️
+                </p></div>
+            </div>
+            <!-- Content Row-->
+     
+        </div>
+        <!-- Footer-->
+<?php include_once('includes/footer.php') ?>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
