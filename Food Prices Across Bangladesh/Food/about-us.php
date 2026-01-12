31c91b26417f8cdec39abcf364f9c078f401d742
Rawnak Tasnim Ruku <tasnim_ruku1152@uits.edu.bd>
2026-01-12 22:51:21 +0600

    Add files via upload

---
+++ Food Prices Across Bangladesh/Food/about-us.php
@@ -0,0 +1,54 @@
+<?php session_start();
+include_once('includes/config.php');
+error_reporting(0);
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Track Food Prices Across Bangladesh  </title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+       <!--  <link href="css/bootstrap.min.css" rel="stylesheet" /> -->
+    </head>
+    <body>
+<?php include_once('includes/header.php');?>
+        <!-- Header-->
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">About Us</h1>
+        
+                </div>
+            </div>
+        </header>
+        <!-- Section-->
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+
+                Check real-time food prices from all 64 districts, from rice and vegetables to meat and cooking oils. Our website keeps you informed on weekly, monthly, and yearly trends, helping you make smart shopping decisions.
+                <br> Whether you're tracking price changes or comparing rates across regions, we've got all the latest updates right at your fingertips.
+
+                </div>
+            </div>
+
+ 
+</div>
+        </section>
+        <!-- Footer-->
+   <?php include_once('includes/footer.php'); ?>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
